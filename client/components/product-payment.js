afbb020f1afbbce67a6acc48478f13afee0994cf
David Xia <apolo2david@yahoo.com>
2019-07-17 16:12:00 +1000

    Update product-payment.js

--- client/components/product-payment.js
+++ client/components/product-payment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
 import Helper from '../../lib/helper';
 // ==== Styles ==============================================
 import { withStyles } from '@material-ui/core/styles';
