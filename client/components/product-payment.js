a924fb24714539b07ade18c02e96853cdb4424ad
David Xia <apolo2david@yahoo.com>
2019-07-17 15:46:23 +1000

    Update product-payment.js

--- client/components/product-payment.js
+++ client/components/product-payment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
 import Helper from '../../lib/helper';
 // ==== Styles ==============================================
 import { withStyles } from '@material-ui/core/styles';
@@ -41,9 +42,70 @@ class ProductPayment extends React.Component {
 		
 		// Display Component
 		return (
-			<div className={classes.panel}>
-				Product Payment
-			</div>
+            <div className={classes.panel}>
+                <h1>Review & Payment</h1>
+			    <div>
+                    <h3 className={classes.panelHeader}>
+                        Tour
+                    </h3>
+                    <Grid container spacing={0} className={classes.panelBody}>
+                        <Grid item xs={12}>Country</Grid>
+                        <Grid item xs={12}>
+                            <Grid item xs={5}>Name</Grid>
+                            <Grid item xs={4}>Date</Grid>
+                            <Grid item xs={3}>Image</Grid>
+                        </Grid>
+                        <Grid item xs={12}>Additional request</Grid>
+                    </Grid>
+                </div>
+			    <div>
+                    <h3 className={classes.panelHeader}>
+                        Traveller Details
+                    </h3>
+                    <Grid container spacing={0} className={classes.panelBody}>
+                        <Grid item xs={12}><h4>Primary Contact</h4></Grid>
+                        <Grid item xs={12}>
+                            <Grid item xs={6}>First Name</Grid>
+                            <Grid item xs={6}>Last Name</Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Grid item xs={6}>Mobile Number</Grid>
+                            <Grid item xs={6}></Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+                <div>
+                    <h3 className={classes.panelHeader}>
+                        Summary
+                    </h3>
+                    <Grid container spacing={0} className={classes.panelBody}>
+                        <Grid item xs={12}>
+                            <Grid item xs={12}><h4>Your Itinerary</h4></Grid>
+                            <Grid item xs={12}>Itinerary blah blah...</Grid>
+                        </Grid>
+                        <Divider />
+                        <Grid item xs={12}>
+                            <Grid item xs={12}><h4>X Days Tour, Price</h4></Grid>
+                            <Grid item xs={12}>Adults ?, Kids ?</Grid>
+                        </Grid>
+                        <Divider />
+                        <Grid item xs={12}>
+                            <Grid item xs={12}><h4>Subtotal</h4></Grid>
+                            <Grid item xs={12}><h2>Grand Total</h2></Grid>
+                        </Grid>
+                        <Divider />
+                        <Grid item xs={12}>Agree?</Grid>
+                    </Grid>
+                </div>
+                <div>
+                    <h3 className={classes.panelHeader}>
+                        Payment
+                    </h3>
+                    <div className={classes.panelBody}>
+                        Payment Content
+                    </div>
+                </div>
+            </div>
 		);
 	}
 }
