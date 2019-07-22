8a110020d2b363363f36168e9167e310a296ca1b
David Xia <apolo2david@yahoo.com>
2019-07-22 16:05:25 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,14 @@ const router = express.Router();
 // GET home page
 router.get('/', (req, res) => {
 	const { hostname } = req;
-	const { PORT, LOCAL } = process.env;
+	const { PORT, LOCAL, REACT_APP_PAYPAL_ENV, REACT_APP_PAYPAL_ID, REACT_APP_PAYPAL_ID_DUMMY, REACT_APP_PAY_DEPOSIT } = process.env;
 	const socketAddress
 		= LOCAL === 'true' ? `http://${hostname}:${PORT}` : `wss://${hostname}`;
-
-	res.render('./index', { socketAddress });
+	const paypalEnv = REACT_APP_PAYPAL_ENV;
+	const paypalId = REACT_APP_PAYPAL_ID;
+	const paypalIdDummy = REACT_APP_PAYPAL_ID_DUMMY;
+	const amountDeposit = REACT_APP_PAY_DEPOSIT ? Number(REACT_APP_PAY_DEPOSIT) : 0;
+	res.render('./index', { socketAddress, paypalEnv, paypalId, paypalIdDummy, amountDeposit });
 });
 
 export default router;
