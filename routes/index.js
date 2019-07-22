1d84dcfd49d27b328e49f6f3b80206e7e665ae59
David Xia <apolo2david@yahoo.com>
2019-07-22 16:23:40 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,10 @@ router.get('/', (req, res) => {
 	const { PORT, LOCAL, REACT_APP_PAYPAL_ENV, REACT_APP_PAYPAL_ID, REACT_APP_PAYPAL_ID_DUMMY, REACT_APP_PAY_DEPOSIT } = process.env;
 	const socketAddress
 		= LOCAL === 'true' ? `http://${hostname}:${PORT}` : `wss://${hostname}`;
-	const paypalEnv = REACT_APP_PAYPAL_ENV;
-	const paypalId = REACT_APP_PAYPAL_ID;
-	const paypalIdDummy = REACT_APP_PAYPAL_ID_DUMMY;
-	const amountDeposit = REACT_APP_PAY_DEPOSIT ? Number(REACT_APP_PAY_DEPOSIT) : 0;
+	const paypalEnv = REACT_APP_PAYPAL_ENV || 'sandbox';
+	const paypalId = REACT_APP_PAYPAL_ID || '';
+	const paypalIdDummy = REACT_APP_PAYPAL_ID_DUMMY || '';
+	const amountDeposit = REACT_APP_PAY_DEPOSIT || '0';
 	res.render('./index', { socketAddress, paypalEnv, paypalId, paypalIdDummy, amountDeposit });
 });
 
