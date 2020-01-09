156d154e323398972c7f73c61f30e62f5663627f
apolo2david@yahoo.com <apolo2david@yahoo.com>
2020-01-09 13:49:23 +1100

    up

--- client/index.js
+++ client/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import InvalidPage from './invalid-page';
 import {BrowserRouter as Router} from 'react-router-dom';
-// Variables
 // Styles
 import '../public/index.css';
+// Variables
 
 window.attachApp = (params) => {
   console.log('>>>>window.attachApp()', params);
